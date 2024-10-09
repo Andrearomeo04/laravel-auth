72e604a03c1fa425e11d15e47c78153fa1f13397
andrearomeo04 <pisco004@gmail.com>
2024-10-09 17:48:14 +0200

    [UPD] migrated the table and imported model and support on seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use App\Models\Project;
+use Illuminate\Support\Str;
 class ProjectSeeder extends Seeder
 {
     /**

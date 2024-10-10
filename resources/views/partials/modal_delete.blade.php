c1339c9370c9c4996672d30b7fe0ee7d26905a33
andrearomeo04 <pisco004@gmail.com>
2024-10-10 02:54:42 +0200

    [UPD] created modal delete button on partials

---
+++ resources/views/partials/modal_delete.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="deleteProjectModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteProjectLabel">Conferma cancellazione</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Sei sicuro di voler eliminare questo progetto?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+        <button type="button" class="btn btn-warning">Elimina</button>
+      </div>
+    </div>
+  </div>
+</div>
